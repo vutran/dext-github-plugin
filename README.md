a28a7a9b406a5038c34a09966602a67d0db954ae
Vu Tran <vu@vu-tran.com>
2016-08-27 12:55:13 -0700

    Initial Commit :sparkles:

---
+++ README.md
@@ -0,0 +1,15 @@
+# dext-github-plugin
+
+> Dext plugin to search for Github repositories.
+
+## Install
+
+$ dpm install dext-github-plugin
+
+## Usage
+
+In Dext, type `gh`, then your query.
+
+## License
+
+MIT © [Vu Tran](https://github.com/vutran/)
---
+++ index.js
@@ -0,0 +1,30 @@
+const got = require('got');
+
+module.exports = {
+  keyword: 'gh',
+  action: 'openurl',
+  output: q => new Promise(resolve => {
+    const opts = {
+      query: {
+        q: `${q} in:name`,
+      },
+      json: true,
+    };
+    got('https://api.github.com/search/repositories', opts)
+      .then(res => {
+        if (res.body) {
+          const items = res.body.items
+            .map(i => Object.assign({}, {
+              title: i.full_name,
+              subtitle: i.description,
+              arg: i.html_url,
+              icon: {
+                path: i.owner.avatar_url,
+              },
+            }))
+            .slice(0, 20);
+          resolve({ items });
+        }
+      });
+  }),
+};
