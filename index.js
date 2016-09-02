a225e7750455bd92c0ba312d0b82f251912b5287
Vu Tran <vu.tran@thermofisher.com>
2016-09-01 23:47:14 -0700

    added icon and helper item

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// GitHub icon by Freepik <http://www.flaticon.com/free-icon/github-mascot-logo-variant_38521#term=github&page=1&position=30>
+
 const got = require('got');
 
 module.exports = {
   keyword: 'gh',
   action: 'openurl',
+  helper: {
+    title: 'Search GitHub repositories.',
+    subtitle: 'Example: gh dext',
+    icon: {
+      path: './icon.png',
+    },
+  },
   execute: q => new Promise(resolve => {
     const opts = {
       query: {
